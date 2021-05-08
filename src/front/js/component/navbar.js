2cdc45adb2450263bee0c836cbbfb9733345e1ab
Jovany1543 <christian.marrero3@gmail.com>
2021-05-08 14:59:07 +0000

    test

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">React</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
