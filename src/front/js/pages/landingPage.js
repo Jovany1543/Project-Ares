4f084927dc9e2289b75ebaa7e39b8025123e03c7
Jovany1543 <christian.marrero3@gmail.com>
2021-05-19 00:39:01 +0000

    Added Landing Page

--- src/front/js/pages/landingPage.js
+++ src/front/js/pages/landingPage.js
@@ -1,24 +1,99 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Jumbotron, Container, Carousel, Row, Item, Caption, Col } from "react-bootstrap";
 import "../../styles/home.scss";
 
 export const LandingPage = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div>
+			<Jumbotron fluid className="w-100">
+				<Container>
+					<h1>Fluid jumbotron</h1>
+					<p>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+				</Container>
+			</Jumbotron>
+
+			<Container>
+				<h4> Guns by Category</h4>
+				<Row>
+					<Col>
+						<img className="d-block w-100" src="https://via.placeholder.com/150" alt="Second slide" />
+					</Col>
+					<Col>
+						<img className="d-block w-100" src="https://via.placeholder.com/150" alt="Second slide" />
+					</Col>
+					<Col>
+						<img className="d-block w-100" src="https://via.placeholder.com/150" alt="Second slide" />
+					</Col>
+				</Row>
+			</Container>
+
+			<Container>
+				<h4>Guns by Use</h4>
+				<Carousel className="content">
+					<Carousel.Item>
+						<Container>
+							<Row>
+								<Col>
+									<img
+										className="d-block w-100"
+										src="https://via.placeholder.com/100"
+										alt="Second slide"
+									/>
+								</Col>
+								<Col>
+									<img
+										className="d-block w-100"
+										src="https://via.placeholder.com/100"
+										alt="Second slide"
+									/>
+								</Col>
+								<Col>
+									<img
+										className="d-block w-100"
+										src="https://via.placeholder.com/100"
+										alt="Second slide"
+									/>
+								</Col>
+							</Row>
+						</Container>
+					</Carousel.Item>
+					<Carousel.Item>
+						<Container>
+							<Row>
+								<Col className="d-flex justify-content-center">
+									<img
+										className="d-block w-55"
+										src="https://via.placeholder.com/100"
+										alt="Second slide"
+									/>
+								</Col>
+								<Col className="d-flex justify-content-center">
+									<img
+										className="d-block w-55"
+										src="https://via.placeholder.com/100"
+										alt="Second slide"
+									/>
+								</Col>
+							</Row>
+						</Container>
+					</Carousel.Item>
+				</Carousel>
+			</Container>
+			<Container>
+				<h4>Useful Links</h4>
+				<Row>
+					<Col>1 of 2</Col>
+					<Col>2 of 2</Col>
+				</Row>
+				<Row>
+					<Col>1 of 2</Col>
+					<Col>2 of 2</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 };
