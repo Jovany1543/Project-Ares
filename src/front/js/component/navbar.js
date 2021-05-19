8ca164961116c534a6fb14a41ba9f1845a868f56
Jovany1543 <christian.marrero3@gmail.com>
2021-05-19 01:17:44 +0000

    modified navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 
 export const MyNavbar = () => {
 	return (
-		<Navbar bg="light" expand="lg">
-			<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+		<Navbar bg="light" expand="false">
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
+			<Form inline className="ml-auto pr-5">
+				<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+				<Button variant="outline-success">Search</Button>
+			</Form>
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="mr-auto">
 					<Nav.Link href="#home">Home</Nav.Link>
@@ -19,10 +24,6 @@ export const MyNavbar = () => {
 						<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
 					</NavDropdown>
 				</Nav>
-				<Form inline>
-					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-					<Button variant="outline-success">Search</Button>
-				</Form>
 			</Navbar.Collapse>
 		</Navbar>
 	);
