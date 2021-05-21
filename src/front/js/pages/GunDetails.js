5d312fe5d2ca332c7ff8e9f1c8a595be7ec1b566
vlmarq <lamarque.van@gmail.com>
2021-05-21 00:49:59 +0000

    working on gun details

--- src/front/js/pages/GunDetails.js
+++ src/front/js/pages/GunDetails.js
@@ -6,31 +6,34 @@ import gunImg from "../../img/hunt.jpeg";
 // import { MiniJumbo } from "../component/MiniJumbo";
 
 // Stylesheet
-// import "../../styles/GunActivity.scss";
+import "../../styles/GunDetails.scss";
 
 // Bootstrap
 import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 
 // FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 
 export const GunDetails = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<>
-			<h1 className="text-center">Gun Category</h1>
-			<h2>Name/Variant: </h2>
-			<h3>Manufacturer: </h3>
-			<Image src={gunImg} />
+		<Container>
+			<h3 className="mt-4">Gun Category</h3>
+			<h1 className="text-center">Name/Variant</h1>
+			<h2 className="text-center">Manufacturer</h2>
+			<Image className="gun-img" src={gunImg} />
+			<div className="text-right">
+				<FontAwesomeIcon className="bookmark-icon my-2" icon={faStar} />
+			</div>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aliquid cumque, esse doloremque
 				veritatis consequuntur aliquam minima laborum magni assumenda suscipit illum autem non facere, qui
 				similique dolore vero repudiandae eligendi voluptatibus quis dolorem! Illo eos rem hic quae nemo id
 				perferendis totam, vero at eaque pariatur, numquam adipisci laudantium.
 			</p>
-			<FontAwesomeIcon icon="faStar" />
-		</>
+		</Container>
 	);
 };
