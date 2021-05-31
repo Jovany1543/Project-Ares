f42ce2497707a88f16ccd02ba751c93b52548857
vlmarq <lamarque.van@gmail.com>
2021-05-31 22:45:24 +0000

    fixed sign up and added form validation

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,11 +8,27 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# Signup route
+@api.route('/signup', methods=['POST'])
+def signup():
+    body = request.json # get the request body content
+    email = request.json.get('email')
+    password = request.json.get('password')
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
+    if body is None:
+        return "The request body is null", 400
+    if not email:
+        return 'You need to specify the email',400
+    if not password:
+        return 'You need to enter a password', 400
+
+    user = User(email=email,password=password,is_active=True)
+
+    db.session.add(user)
+    db.session.commit()
+    payload = {
+        'msg': 'Thank you for registering. Your account has been added successfully.',
+        'user': user.serialize()
     }
 
-    return jsonify(response_body), 200
+    return jsonify(payload), 200
--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export const Signup = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm();
+
+	const onSubmit = data => {
+		console.log(data);
+	};
+
 	return (
 		<div className="container-fluid">
 			<div className="row d-flex main-content text-center w-50 rounded shadow-lg my-5 mx-auto py-4">
@@ -11,7 +22,12 @@ export const Signup = () => {
 							<h1 className="font-weight-light ">Sign up</h1>
 						</div>
 						<div className="row justify-content-center">
-							<form control="" className="form-group p-2 w-100">
+							<form
+								method="POST"
+								action="/signup"
+								control=""
+								className="form-group p-2 w-100"
+								onSubmit={handleSubmit(onSubmit)}>
 								<div className="row justify-content-between pt-4">
 									<span>
 										<input
@@ -20,7 +36,9 @@ export const Signup = () => {
 											id="fname"
 											className="form__input border-top-0 border-left-0 border-right-0 border-bottom w-100"
 											placeholder="First Name"
+											{...register("fname", { required: "Please enter your first name" })}
 										/>
+										{errors.fname && <span className="text-danger">{errors.fname.message}</span>}
 									</span>
 									<span>
 										<input
@@ -29,17 +47,27 @@ export const Signup = () => {
 											id="lname"
 											className="form__input border-top-0 border-left-0 border-right-0 border-bottom w-100"
 											placeholder="Last Name"
+											{...register("lname", { required: "Please enter your last name" })}
 										/>
+										{errors.lname && <span className="text-danger">{errors.lname.message}</span>}
 									</span>
 								</div>
 								<div className="row justify-content-center pt-4">
 									<input
 										type="text"
-										name="username"
-										id="username"
+										name="email"
+										id="email"
 										className="form__input border-top-0 border-left-0 border-right-0 border-bottom w-100"
 										placeholder="Email Address"
+										{...register("email", {
+											required: "E-mail is required",
+											pattern: {
+												value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+												message: "Enter a valid email address"
+											}
+										})}
 									/>
+									{errors.email && <span className="text-danger">{errors.email.message}</span>}
 								</div>
 								<div className="row justify-content-center pt-4">
 									<input
@@ -48,22 +76,12 @@ export const Signup = () => {
 										id="password"
 										className="form__input border-top-0 border-left-0 border-right-0 border-bottom w-100"
 										placeholder="Password"
+										{...register("password", {
+											required: "Password is required.",
+											minLength: { value: 8, message: "Password must be 8 characters or more." }
+										})}
 									/>
-								</div>
-								<div className="row justify-content-center pt-4">
-									<input
-										type="password"
-										name="password"
-										id="password"
-										className="form__input border-top-0 border-left-0 border-right-0 border-bottom w-100"
-										placeholder="Confirm Password"
-									/>
-								</div>
-								<div className="row my-4 align-items-center pt-4">
-									<input type="checkbox" name="remember_me" id="remember_me" className="mr-1" />
-									<label className="text-black-50 mb-0" htmlFor="remember_me">
-										Remember Me
-									</label>
+									{errors.password && <span className="text-danger">{errors.password.message}</span>}
 								</div>
 								<div className="row justify-content-center">
 									<input
